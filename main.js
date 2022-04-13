88ad81ee12419066c8b6c3daa160d1fffb1eef4c
Carlos Silva <carlos.eduardo.k2103@gmail.com>
2022-04-12 22:43:50 -0300

    12/04 CarlosPC

--- main.js
+++ main.js
@@ -15,25 +15,59 @@ for (const link of links) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Swiper */
-const swiper = new Swiper('.swiper', {  
+const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
   mousewheel: {
-    invert: false,
+    invert: false
   },
   keyboard: true
-});
+})
+
+/* ScrollReveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+/* Button back to top */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', () => {
+  backToTop()
+  changeHeaderWhenScroll()
+})
